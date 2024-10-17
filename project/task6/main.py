fe6d8857ac6bdb8616b2ad88c278d59d694fa2c1
Homka122 <kombaev02@gmail.com>
2024-10-17 12:00:22 +0300

    feat: implement hellings_based_cfpq
    Signed-off-by: Homka122 <kombaev02@gmail.com>

--- project/task6/main.py
+++ project/task6/main.py
@@ -1,5 +1,10 @@
 import networkx as nx
-from pyformlang.cfg import Production, CFG
+from autopep8 import Tuple
+from networkx.classes import MultiDiGraph
+from pyformlang.cfg import Production, CFG, Epsilon
+
+from tests.autotests.constants import LABEL
+
 
 def cfg_to_weak_normal_form(cfg: CFG) -> CFG:
     normal_form = cfg.to_normal_form()
@@ -9,15 +14,62 @@ def cfg_to_weak_normal_form(cfg: CFG) -> CFG:
         # Add eps word
         start_symbol = normal_form.start_symbol
 
-        cfg_eps = CFG({start_symbol}, None, start_symbol, [Production(start_symbol, [])])
+        cfg_eps = CFG(
+            {start_symbol}, None, start_symbol, [Production(start_symbol, [])]
+        )
         weak = normal_form.union(cfg_eps)
 
     return weak
 
+
 def hellings_based_cfpq(
-  cfg: CFG,
-  graph: nx.DiGraph,
-  start_nodes: set[int] = None,
-  final_nodes: set[int] = None,
+    cfg: CFG,
+    graph: nx.DiGraph,
+    start_nodes: set[int] = None,
+    final_nodes: set[int] = None,
 ) -> set[tuple[int, int]]:
-   pass
+    def get_edge_label(first: int, second: int) -> str | None:
+        edge = graph.get_edge_data(first, second)
+        if edge:
+            return edge[0]["label"]
+
+        return None
+
+    weak = cfg_to_weak_normal_form(cfg)
+
+    r = list()
+    for first in list(graph):
+        for second in list(graph):
+            for production in weak.productions:
+                if first == second and Epsilon in production.body:
+                    r.append((production.head, first, first))
+
+                label = get_edge_label(first, second)
+                if len(production.body) == 1 and production.body[0].value == label:
+                    r.append((production.head, first, second))
+
+    new = r.copy()
+    while new:
+        (N, n, m) = new.pop(0)
+        for (M, k, p) in r:
+            for production in weak.productions:
+                if p == n and len(production.body) == 2 and production.body[0].value == M and production.body[1] == N:
+                    if (production.head, k, m) not in r:
+                        new.append((production.head, k, m))
+                    if (production.head, k, m) not in r:
+                        r.append((production.head, k, m))
+
+        for (M, k, p) in r:
+            for production in weak.productions:
+                if k == m and len(production.body) == 2 and production.body[0].value == N and production.body[1] == M:
+                    if (production.head, m, p) not in r:
+                        new.append((production.head, m, p))
+                    if (production.head, m, p) not in r:
+                        r.append((production.head, m, p))
+
+    resultt = set()
+    for (N, f, s) in r:
+        if f in start_nodes and s in final_nodes:
+            resultt.add((f, s))
+
+    return resultt
--- tests/autotests/test_task06.py
+++ tests/autotests/test_task06.py
@@ -9,7 +9,7 @@ from cfpq_concrete_cases import CaseCFPQ, CASES_CFPQ
 
 # Fix import statements in try block to run tests
 try:
-    from project.task6 import hellings_based_cfpq
+    from project.task6.main import hellings_based_cfpq
 except ImportError:
     pytestmark = pytest.mark.skip("Task 6 is not ready to test!")
 
